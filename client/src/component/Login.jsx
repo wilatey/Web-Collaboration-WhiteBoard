008d2631e6047da36cc86c2abc19d17590d98e2b
Wilatey <wilsonlam085@gmail.com>
2025-08-25 17:41:50 +0800

    minor adjustment on Login Page
    Show invalid username and navigate to dashboard

--- client/src/component/Login.jsx
+++ client/src/component/Login.jsx
@@ -1,23 +1,24 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-export function Login({ onSubmit }) {
+export function Login() {
     const [username, setUsername] = useState("")
+    const [error, setError] = useState("");
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const handleDirect = (e) => { 
         e.preventDefault();
-        onSubmit(username);
-        setUsername( e.target.value );
-        navigate("/dashboard");
+        if (username.trim() === "") {
+            setError("Please enter a valid username");
+            return;
+        }
+        setError("");
+        navigate("/dashboard", { state: { username } });
     }
 
     return (
         <>
-            {/* create a form and input for username, then submit it and call onSubmit with the username 
-            After that, it will redirect to the dashboard
-            */}
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleDirect}>
                     <div className="w-full px-2 py-1 ">
                         <div>
                         <input
@@ -26,10 +27,11 @@ export function Login({ onSubmit }) {
                             placeholder="Type name"
                             id="username"
                             name="username"
-                            onChange={{handleSubmit}}
+                            onChange={(e) => setUsername(e.target.value)}
                             className="py-1 p-20 justify-center text-center text-2xl *:rounded-md border border-input bg-background focus:outline-none focus:ring-1"
                             />
-                        </div>
+                    </div>
+                    {error && <p className="text-red-500 mt-2">{error}</p>}
                     <button type="submit" className="cosmic-button border w-200 my-5"> Submit form </button>
                 </div>
             </form>
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -8,7 +8,7 @@ export const LoginPage = () => {
             <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
                 <div className="container relative px-15 py-10 bg-primary border border-border rounded-full">
                     <h1 className="text-5xl m-2 font-serif">Welcome to Collaborative WhiteBoard!</h1>
-                    <p className="text-2xl m-4 font-light font-sans">Please signup with your username~</p>
+                    <p className="text-2xl m-4 font-light font-sans">Please signup with your username</p>
                     <Login />
                 </div>
             </div>
